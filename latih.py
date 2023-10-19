5449e3aea7d13d77dabe3a1ad240b927ac561f33
Rifaldi16lk <148313270+Rifaldi16lk@users.noreply.github.com>
2023-10-19 14:13:39 +0700

    Add files via upload

---
+++ latih.py
@@ -0,0 +1,15 @@
+a = 5
+b = 3
+hasil = a + b
+print("Hasil penjumlahan a dan b adalah:", hasil)
+#perkalian 2 variabel
+x = 7
+y = 4
+hasil = x * y
+print("Hasil perkalian x dan y adalah:", hasil)
+#Menghitung Rata-rata Sejumlah Variabel
+nilai1 = 85
+nilai2 = 92
+nilai3 = 78
+rata_rata = (nilai1 + nilai2 + nilai3) / 3
+print("Rata-rata dari ketiga nilai adalah:", rata_rata)
